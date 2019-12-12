f387731540bd63d5d7f000b29e43845d8bddd498
Minna Seppälä <mimsi.seppala@gmail.com>
2019-12-12 19:16:45 +0200

    css fixes

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,18 @@
 import React, {useState} from 'react';
 import '../css/App.css';
 import { Container } from '@material-ui/core';
-<<<<<<< HEAD
 import FrontPage from './FrontPage'
 import BirthPage from './BirthPage';
 import DeathPage from './DeathPage';
-=======
->>>>>>> 89aab6a6a81461f3252697bbe87fcc86e8780a97
 
 const App = () => {
   const [show, setShow] = useState('frontpage')
 
   return (
-<<<<<<< HEAD
     <Container maxWidth="sm">    
       {show === 'frontpage' && <FrontPage setShow={setShow}/>}
       {show === 'birthpage' && <BirthPage setShow={setShow}/>}
       {show === 'deathpage' && <DeathPage setShow={setShow}/>}
-=======
-    <Container maxWidth="sm">
-      <div>test test</div>
->>>>>>> 89aab6a6a81461f3252697bbe87fcc86e8780a97
     </Container>
   );
 }
--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { Card, Button } from '@material-ui/core';
-import {PersonOutline, PersonAdd} from '@material-ui/icons';
+import { Card, CardContent } from '@material-ui/core';
+import { PersonOutline, PersonAdd } from '@material-ui/icons';
+import '../css/FrontPage.css';
 
 const FrontPage = (props) => {
   const { setShow } = props
 
   return (
     <div>
+      <h3 className='card-text'>M Government Report</h3>
       <Card onClick={() => setShow('birthpage')}>
-        <PersonAdd/>
+        <CardContent>
+          <PersonAdd className='card-icon'/>
+          <div className='card-text'>Report a birth</div>
+        </CardContent> 
       </Card>
       <Card onClick={() => setShow('deathpage')}>
-        <PersonOutline/>
+        <CardContent>
+          <PersonOutline className='card-icon'/>
+          <div className='card-text'>Report a death</div>
+        </CardContent> 
       </Card>
     </div>
   );
