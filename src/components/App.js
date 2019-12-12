a643811d232580ddf007f8b56ba87a6038b4fe2b
Jere Suikkila <jere@suikkila.com>
2019-12-12 19:02:33 +0200

    Fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,20 @@
-import React, {useState} from 'react';
-import '../css/App.css';
-import { Container } from '@material-ui/core';
-<<<<<<< HEAD
-import FrontPage from './FrontPage'
-import BirthPage from './BirthPage';
-import DeathPage from './DeathPage';
-=======
->>>>>>> 89aab6a6a81461f3252697bbe87fcc86e8780a97
+import React, { useState } from "react";
+import "../css/App.css";
+import { Container } from "@material-ui/core";
+import FrontPage from "./FrontPage";
+import BirthPage from "./BirthPage";
+import DeathPage from "./DeathPage";
 
 const App = () => {
-  const [show, setShow] = useState('frontpage')
+  const [show, setShow] = useState("frontpage");
 
   return (
-<<<<<<< HEAD
-    <Container maxWidth="sm">    
-      {show === 'frontpage' && <FrontPage setShow={setShow}/>}
-      {show === 'birthpage' && <BirthPage setShow={setShow}/>}
-      {show === 'deathpage' && <DeathPage setShow={setShow}/>}
-=======
     <Container maxWidth="sm">
-      <div>test test</div>
->>>>>>> 89aab6a6a81461f3252697bbe87fcc86e8780a97
+      {show === "frontpage" && <FrontPage setShow={setShow} />}
+      {show === "birthpage" && <BirthPage setShow={setShow} />}
+      {show === "deathpage" && <DeathPage setShow={setShow} />}
     </Container>
   );
-}
+};
 
 export default App;
