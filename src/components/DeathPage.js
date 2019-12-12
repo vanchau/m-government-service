6b8a1126b7f488b48c5990f563740c0447983945
Jere Suikkila <jere@suikkila.com>
2019-12-13 01:33:27 +0200

    Progressbar for death

--- src/components/DeathPage.js
+++ src/components/DeathPage.js
@@ -2,20 +2,31 @@ import React from "react";
 import Layout from "./Layout";
 import DeathForm from "./DeathForm";
 import PageActions from "./PageActions";
+import ProgressBar from "./ProgressBar";
 
 const DeathPage = props => {
   const { setShow, deathInfo, setDeathInfo } = props;
+  const numberOfSteps = 2;
 
-  const Footer = () => {
+  const Footer = ({ step }) => {
+    const nextText = step === numberOfSteps ? "Confirm" : "Next";
     return (
-      <PageActions info={deathInfo} setInfo={setDeathInfo} setShow={setShow}/>
+      <PageActions
+        info={deathInfo}
+        setInfo={setDeathInfo}
+        setShow={setShow}
+        previousText="Previous"
+        nextText={nextText}
+      />
     );
   };
 
   return (
     <Layout header="Report a death">
+      <ProgressBar current={deathInfo.step} total={numberOfSteps} />
+
       <DeathForm deathInfo={deathInfo} setDeathInfo={setDeathInfo} />
-      <Footer />
+      <Footer step={deathInfo.step} />
     </Layout>
   );
 };
