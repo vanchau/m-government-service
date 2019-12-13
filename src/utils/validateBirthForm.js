34ec100b72381731ec1661b41b6c9a3e8ee8c27b
vchau <van.chau@aalto.fi>
2019-12-13 21:21:43 +0200

    Modified id requirements

--- src/utils/validateBirthForm.js
+++ src/utils/validateBirthForm.js
@@ -6,21 +6,21 @@ const validateBirthForm = (values) => {
     if (!values.reporterId) {
       errors.reporterId = 'Please give your identification number.'
     }
-    else if (!validator.isAlphanumeric(values.reporterId, 'en-ZA')) {
-      errors.reporterId = 'Invalid characters in your identification number. Only letters a-z and numbers 0-9 are allowed.'
+    else if (!validator.isNumeric(values.reporterId)) {
+      errors.reporterId = 'Invalid characters in your identification number. Only numbers are allowed.'
     }
-    else if (!validator.isLength(values.reporterId, {min: 8, max: 8} )) {
-      errors.reporterId = "Your identification number should be 8 characters long."
+    else if (!validator.isLength(values.reporterId, {min: 11, max: 11} )) {
+      errors.reporterId = "Your identification number should be 11 characters long."
     }
 
     if (!values.motherId) {
       errors.motherId = 'Please give the identification number of the mother.'
     }
-    else if (!validator.isAlphanumeric(values.motherId, 'en-ZA')) {
-      errors.motherId = 'Invalid characters in the identification number of the mother. Only letters a-z and numbers 0-9 are allowed.'
+    else if (!validator.isNumeric(values.motherId)) {
+      errors.motherId = 'Invalid characters in the identification number of the mother. Only numbers are allowed.'
     }
-    else if (!validator.isLength(values.motherId, {min: 8, max: 8} )) {
-      errors.motherId = "Identification number of the mother should be 8 characters long."
+    else if (!validator.isLength(values.motherId, {min: 11, max: 11} )) {
+      errors.motherId = "Identification number of the mother should be 11 characters long."
     }
   }
   else if (values.step === 2) {
--- src/utils/validateDeathForm.js
+++ src/utils/validateDeathForm.js
@@ -5,21 +5,21 @@ const validateDeathForm = (values) => {
   if (!values.reporterId) {
     errors.reporterId = 'Please give your identification number.'
   }
-  else if (!validator.isAlphanumeric(values.reporterId, 'en-ZA')) {
-    errors.reporterId = 'Invalid characters in your identification number. Only letters a-z and numbers 0-9 are allowed.'
+  else if (!validator.isNumeric(values.reporterId)) {
+    errors.reporterId = 'Invalid characters in your identification number. Only numbers are allowed.'
   }
-  else if (!validator.isLength(values.reporterId, {min: 8, max: 8} )) {
-    errors.reporterId = "Your identification number should be 8 characters long."
+  else if (!validator.isLength(values.reporterId, {min: 11, max: 11} )) {
+    errors.reporterId = "Your identification number should be 11 characters long."
   }
 
   if (!values.deceasedId) {
     errors.deceasedId = 'Please give the identification number of the deceased.'
   }
-  else if (!validator.isAlphanumeric(values.deceasedId, 'en-ZA')) {
-    errors.deceasedId = 'Invalid characters in the identification number of the deceased. Only letters a-z and numbers 0-9 are allowed.'
+  else if (!validator.isNumeric(values.deceasedId)) {
+    errors.deceasedId = 'Invalid characters in the identification number of the deceased. Only numbers are allowed.'
   }
-  else if (!validator.isLength(values.deceasedId, {min: 8, max: 8} )) {
-    errors.deceasedId = "The identification number of the deceased should be 8 characters long."
+  else if (!validator.isLength(values.deceasedId, {min: 11, max: 11} )) {
+    errors.deceasedId = "The identification number of the deceased should be 11 characters long."
   }
   if (!values.timeOfDeath) {
     errors.timeOfDeath = 'Please enter the date of death.'
