fa843df9a972a0d915a273a53de9a82c8670b3b3
vchau <van.chau@aalto.fi>
2019-12-13 21:25:12 +0200

    Add help information to first step of birth form

--- src/components/BirthPage.js
+++ src/components/BirthPage.js
@@ -3,6 +3,7 @@ import Layout from "./Layout";
 import BirthForm from "./BirthForm";
 import PageActions from "./PageActions";
 import ProgressBar from "./ProgressBar";
+import Help from "./Help";
 
 const BirthPage = props => {
   const { type, setType, setShow, birthInfo, setBirthInfo } = props;
@@ -22,12 +23,12 @@ const BirthPage = props => {
         nextText={nextText}
         setErrors={setErrors}
       />
-
     );
   };
 
   return (
     <Layout header="Report a birth">
+      <Help>Identification numbers are unique for each person and can be found from the national identity card.</Help>
       <ProgressBar current={birthInfo.step} total={numberOfSteps} />
       <BirthForm type={type} setType={setType} birthInfo={birthInfo} setBirthInfo={setBirthInfo} />
       {Object.keys(errors).length !== 0 && <p style={{color: 'red', fontSize: '10', marginTop: '2rem'}}>{errors[Object.keys(errors)[0]]}</p>}
